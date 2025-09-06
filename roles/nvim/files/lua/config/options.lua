3925dc7ab60b5bcdf333b310232fed9c1816bf58
Anirudh Aggarwal <16053724+ani1797@users.noreply.github.com>
2025-09-05 22:04:57 -0400

    Add Neovim configuration files for options and completion setup

---
+++ roles/nvim/files/lua/config/options.lua
@@ -0,0 +1,15 @@
+-- Neovim options for UI and editing behavior
+-- Loads before plugins and keymaps
+
+local opt = vim.opt
+
+-- Show absolute line numbers
+opt.number = true
+-- Show relative line numbers
+opt.relativenumber = true
+-- Set tabstop, shiftwidth, and expandtab for 2-space tabs
+opt.tabstop = 2
+opt.shiftwidth = 2
+opt.expandtab = true
+
+-- Add any other early options here
--- roles/nvim/files/lua/plugins/lspconfig.lua
+++ roles/nvim/files/lua/plugins/lspconfig.lua
@@ -2,7 +2,13 @@ return {
   "neovim/nvim-lspconfig",
   config = function()
     local lspconfig = require("lspconfig")
-    -- Example: Enable lua_ls for Lua
+    -- Enable lua_ls for Lua
     lspconfig.lua_ls.setup {}
+
+    -- Enable bash-language-server for Bash/Zsh
+    lspconfig.bashls.setup {}
+
+    -- Enable ansible-language-server for Ansible
+    lspconfig.ansiblels.setup {}
   end,
 }
